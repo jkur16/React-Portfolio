41d91a2ebda57d64207caa9fbab9a01ff0422c00
Jamie Kuras <jamiekuras@gmail.com>
2021-09-17 16:19:40 -0400

    added resume data

---
+++ src/resumeData.js
@@ -0,0 +1,87 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Jamie Kuras",
+    "role": "Frontend Developer",
+    "linkedinId":"https://www.linkedin.com/in/jamie-kuras-08859637/",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/jamie-kuras-08859637/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/jkur16",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"https://rbhatia46.github.io",
+    "education":[
+      {
+        "UniversityName":"Michigan State University",
+        "specialization":"Coding Bootcamp",
+        "MonthOfPassing":"Sep",
+        "YearOfPassing":"2021",
+      },
+      {
+        "UniversityName":"Eastern Michigan University",
+        "specialization":"Electronic Media and Film",
+        "MonthOfPassing":"Apr",
+        "YearOfPassing":"2008",
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"State of Michigan",
+        "specialization":"Departmental Analyst",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"",
+      },
+      {
+        "CompanyName":"WILX TV-10",
+        "specialization":"Sports Reporter",
+        "MonthOfLeaving":"Jun",
+        "YearOfLeaving":"2015",
+        "Achievements":"2015 Michigan Association of Broadcasters Broadcast Excellence Award 'Best Sportscast'"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+  }
+  
+  export default resumeData
